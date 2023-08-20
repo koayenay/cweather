2778ef8ac729e4e860b6c87e31d383f57b1dcac7
koayenay <koayenay@gmail.com>
2023-08-19 23:19:28 -0700

    fetching data

--- src/App.js
+++ src/App.js
@@ -71,6 +71,7 @@ class App extends React.Component {
         // this.setState({ weather: weatherData.daily })
         // this.setState({ weather: weatherData.daily })
         // this.setState({ weather: weatherData.daily })
+        // this.setState({ weather: weatherData.daily })
       } catch (err) {
         console.err(err)
       } finally {
